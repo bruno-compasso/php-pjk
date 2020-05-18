7cbebcf0fbe72c18ba5fd63151c59ed22acebae1
bruno pereira <bruno.pereira@compasso.com.br>
2020-05-18 14:34:08 -0300

    ...

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
   $age = "23";
   $foo = TRUE;
   $floatNumber = 1.24;
+  $hobbies = array("procrastinating", "sleeping", "gaming");
 
   echo gettype($name);
 
@@ -21,7 +22,18 @@
     echo "<p>$foo</p>";
     echo (int) (25 / 7);
     echo "<p>$floatNumber</p>";
+    echo '<p>this is a simple string</p>';
+
+    echo "You can also have embedded newlines in
+    strings this way as it is
+    okay to do";
   }
+
+  echo "<p>Hi my name is ${name}, and my favorite hobby is $hobbies[1]</p>";
+  echo "<p>Hey " . "there, " . "my name is $name</p>";
+  echo "<p><i>\$hobbies</i> is an " . gettype($hobbies) . "</p>";
+  echo serialize($hobbies);
+  var_dump($hobbies);
   ?>
 </body>
 
